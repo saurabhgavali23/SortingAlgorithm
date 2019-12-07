ac75fb2c3bb77b3eff050081c9fea09acc08a084
saurabhgavali23 <gavalisaurabh02@gmail.com>
2019-12-07 17:01:57 +0530

    ADDED: Arithmatic Operation

--- ArithmaticOperation.sh
+++ ArithmaticOperation.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -x
 
-read -p "Enter The Three Values " a b c
+read -p "Enter The First Value " a
+read -p "Enter The Second Value" b
+read -p "Enter The Third Value" c
 
+result=$((($a+$b)+$c))
+echo $result
